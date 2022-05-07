c1b8ec35fd1619fa03185b99264d560d2e9ee870
riff3r <arif4alll@gmail.com>
2022-05-07 08:11:50 +0600

    Inventory API Added

--- index.js
+++ index.js
@@ -10,6 +10,31 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g0j0y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const bookCollection = client.db("bookzilla").collection("book");
+    await client.connect();
+
+    app.get("/inventory", async (req, res) => {
+      const query = {};
+      const cursor = bookCollection.find(query);
+
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
 // Default server
 app.get("/", (req, res) => {
   res.send("Bookzilla!!");
